220d3f020c8799ee4763af66197154d0fb3c1cd6
jhs512 <jangka512@gmail.com>
2021-07-27 18:50:22 +0900

    ssg 작업

--- foot.php
+++ foot.php
@@ -1,6 +1,6 @@
 </main>
   <footer class="bottom-bar h-20 flex-shrink-0 flex justify-center items-center bg-black text-white">
-    <a href="/" class="logo">로고</a>
+    <a href="/index.ssg.php" class="logo">로고</a>
   </footer>
 </div>
 </body>
